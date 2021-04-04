e65070bcb4cd9a9a8a65ee2ec7ab5e8e51d84f8c
Podojohn <johnpodogorianiotis@gmail.com>
2021-04-04 20:24:32 +0300

    CorrectMerge I think

--- MakeItWorkYouUselessCunt/Controllers/DisplayController.cs
+++ MakeItWorkYouUselessCunt/Controllers/DisplayController.cs
@@ -103,6 +103,80 @@ namespace ManagementSystemVersionTwo.Controllers
             return View(data);
         }
 
+        public ActionResult ViewAllProjects(string title, string orderBy, string depID, string status)
+        {
+            var data = _data.AllProjects();
+            if (!string.IsNullOrEmpty(title))
+            {
+                data = _data.FindProjectByTitle(title, data);
+            }
+            if (!string.IsNullOrEmpty(orderBy))
+            {
+                data = _data.SortProject(orderBy, data);
+            }
+            if (!string.IsNullOrEmpty(depID))
+            {
+                data = _data.GetProjectsPerDepartmentForSort(int.Parse(depID), data);
+            }
+            if (!string.IsNullOrEmpty(status))
+            {
+                data = _data.StatusProject(status, data);
+            }
+
+            List<SelectListItem> listItems = new List<SelectListItem>();
+            listItems.Add(new SelectListItem
+            {
+                Text = "Title",
+                Value = "Title"
+            });
+            listItems.Add(new SelectListItem
+            {
+                Text = "Employees",
+                Value = "Employees"
+            });
+            listItems.Add(new SelectListItem
+            {
+                Text = "Start Date",
+                Value = "Start Date"
+            });
+            listItems.Add(new SelectListItem
+            {
+                Text = "End Date",
+                Value = "End Date"
+            });
+            ViewBag.SortOptions = listItems;
+
+            List<SelectListItem> statusItems = new List<SelectListItem>();
+            statusItems.Add(new SelectListItem
+            {
+                Text = "Finished",
+                Value = "Finished"
+            });
+            statusItems.Add(new SelectListItem
+            {
+                Text = "Not Finished",
+                Value = "Not Finished"
+            });
+            ViewBag.StatusOptions = statusItems;
+
+            var titleForAutoComplete = _data.GetProjectNamesForAutocomplete();
+            ViewBag.Names = titleForAutoComplete;
+
+            List<SelectListItem> departmentItems = new List<SelectListItem>();
+            var allDepartments = _data.AllDepartments();
+            foreach (var items in allDepartments)
+            {
+                departmentItems.Add(new SelectListItem
+                {
+                    Text = items.City,
+                    Value = $"{items.ID}"
+                });
+            }
+            ViewBag.DepartmentOptions = departmentItems;
+
+            return View(data);
+        }
+
         public ActionResult DetailsDepartment(int? id)
         {
 
