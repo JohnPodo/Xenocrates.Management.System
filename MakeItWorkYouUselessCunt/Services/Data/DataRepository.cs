2018a9b81e675b79c89c27c58053214366e76ec7
Podojohn <johnpodogorianiotis@gmail.com>
2021-04-08 16:55:56 +0300

    Correct merge

--- MakeItWorkYouUselessCunt/Services/Data/DataRepository.cs
+++ MakeItWorkYouUselessCunt/Services/Data/DataRepository.cs
@@ -348,7 +348,7 @@ namespace ManagementSystemVersionTwo.Services.Data
 
         public List<IdentityRole> GetRoleByName(string searchString, List<IdentityRole> roles) => roles.Where(x => x.Name.Contains(searchString)).ToList();
 
-        public List<Project> FindProjectsPerWorker(int id) => _context.Projects.Include(s => s.WorkersInMe).Where(p => p.WorkersInMe.FirstOrDefault(w => w.WorkerID == id) != null).ToList();
+       
 
         public void Dispose()
         {
